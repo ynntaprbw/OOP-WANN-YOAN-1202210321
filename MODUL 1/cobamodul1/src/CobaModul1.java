2804888c39cbdae9c0dacb36cb74d439a8d2f6b1
ynntaprbw <ninditayoan@student.telkomuniversity.ac.id>
2022-10-31 08:37:33 +0700

    feat : OOP_WANN_YOAN_1202210321_SSMODUL1

--- MODUL 1/cobamodul1/src/CobaModul1.java
+++ MODUL 1/cobamodul1/src/CobaModul1.java
@@ -1,5 +1,10 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class CobaModul1 {
+    public static void main(String[] args) {
+        String firstName = "John ";
+        String lastName = "Doe";
+        String fullName = firstName + lastName;
+        System.out.println(fullName); 
+      }
     }
-}
+
+    
